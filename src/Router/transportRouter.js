76c38d1a960b75eb2ee37d6bd1d4db714271ff0e
Abduqodir-12 <abduqodirabdujabbarov60@gmail.com>
2024-11-13 18:35:54 +0500

    zdfgxdxgb

--- src/Router/transportRouter.js
+++ src/Router/transportRouter.js
@@ -4,5 +4,7 @@ const middlewear = require('../midleware/midleware')
 const tranportRouter = require('express').Router()
 
 tranportRouter.post('/', middlewear, transportCtrl.createTransport)
+tranportRouter.get('/', transportCtrl.getAllTransport)
+tranportRouter.get('/:id', transportCtrl.getOneTransport)
 
 module.exports = tranportRouter
